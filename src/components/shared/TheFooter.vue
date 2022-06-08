7f51457dc7fea2eee99a9c5d4ac0b5ead779625b
visarzejna <visarzejna@gmail.com>
2022-06-09 00:21:23 +0200

    Project structure setup

---
+++ src/components/shared/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>KosMeetuper</strong>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+</style>
